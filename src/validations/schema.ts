ab85daf3a8d8ff30fe2240ca69d34e148b09b1b7
Prarthanaxdev <Prarthanab@mindfiresolutions.com>
2026-01-09 20:30:05 +0530

    few chnages on schema name not required

--- src/validations/schema.ts
+++ src/validations/schema.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 // ============== AUTH SCHEMAS ==============
 
 export const RegisterSchema = z.object({
-  name: z.string().min(2, 'Name must be at least 2 characters'),
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
 });
@@ -29,7 +28,6 @@ export const CreateTransactionSchema = z.object({
   categoryId: z.string().min(1, 'Category ID required'),
   amount: z.number().positive('Amount must be > 0'),
   type: z.enum(['income', 'expense']),
-  date: z.string(),
   description: z.string().optional(),
 });
 
